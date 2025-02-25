6ed8476321bbec90495a643356d0d279c26b6b59
sunny <kushsunny2002@gmail.com>
2025-02-25 23:59:30 +0530

    "react"

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement("div",{id: "parent"},
+    React.createElement("div",{id: "clild"},
+        [React.createElement("h1",{},"This is h1 tag inside the two div tags"),React.createElement("h2",{},"This is h2 tag")
+    ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
